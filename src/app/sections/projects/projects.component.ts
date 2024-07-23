faddf45569683367aec016c813b41c53751d2713
adityayadav3005 <adityayadav300645@gmail.com>
2024-07-23 19:51:43 +0530

    Projects and achivements

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectsCardComponent } from "../../components/projects-card/projects-card.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ProjectsCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
